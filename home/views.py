36f5cc8754ad77cbaf5e0ab5d5a174f53e212bbc
Matheus Oliveira <m4th3vz@gmail.com>
2025-01-29 22:45:59 -0300

    Novo template

--- home/views.py
+++ home/views.py
@@ -3,27 +3,3 @@ from django.views.generic import TemplateView
 
 class IndexViews(TemplateView):
     template_name = 'home/index.html'
-
-class PythonViews(TemplateView):
-    template_name = 'home/python.html'
-
-class AplicacaoWebViews(TemplateView):  
-    template_name = 'home/aplicacao_web.html'
-
-class HtmlCssViews(TemplateView):
-    template_name = 'home/html_css.html'
-
-class HospedagemViews(TemplateView):
-    template_name = 'home/hospedagem.html'
-
-class AboutViews(TemplateView):
-    template_name = 'home/about.html'
-
-class Page1Views(TemplateView):
-    template_name = 'home/page1.html'
-
-class Page2Views(TemplateView):
-    template_name = 'home/page2.html'
-
-class Page3Views(TemplateView):
-    template_name = 'home/page3.html'
