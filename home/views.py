b1c4d255de97dbd869dc3dcca7e666961a5d1394
Matheus Oliveira <m4th3vz@gmail.com>
2025-01-31 21:48:44 -0300

    Páginas adicionais

--- home/views.py
+++ home/views.py
@@ -3,3 +3,15 @@ from django.views.generic import TemplateView
 
 class IndexViews(TemplateView):
     template_name = 'home/index.html'
+
+class BolsoViews(TemplateView):
+    template_name = 'home/bolso.html'
+
+class PythonViews(TemplateView):
+    template_name = 'home/python.html'
+
+class GithubViews(TemplateView):
+    template_name = 'home/github.html'
+
+class ChatbotViews(TemplateView):
+    template_name = 'home/chatbot.html'
